56753ddaf1ed8a412d1d33f9c92256a22906bd2e
juanzuloaga <jzuloaga65@gmail.com>
2023-04-10 11:57:48 -0600

    Original files Juan Zuloaga

---
+++ to_run_intro_espacial.R
@@ -0,0 +1,7 @@
+
+setwd("C:/Talleres_Ecodiversa_R/")
+rmarkdown::render(input = "./Intro_espacial/Intro_espacial.Rmd",
+                  output_file =  "Intro_espacial.html",
+                  output_dir = "./Intro_espacial")
+
+
